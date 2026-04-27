62c8ed040a2aafc2f26af44ef34692a03688fb39
Hasan Dayoub <hasan123dayoub@gmail.com>
2026-04-28 00:57:15 +0300

    fix a controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,10 +9,4 @@ use App\Models\Project;
 abstract class Controller
 {
     use AuthorizesRequests;
-    public function index()
-    {
-        return Cache::tags(['projects'])->remember('open_projects_page_' . request('page', 1), 3600, function () {
-            return Project::where('status', 'open')->latest()->paginate(10);
-        });
-    }
 }
